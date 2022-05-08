7801d1b4061c1ec67ac20245480d24cea1024fd1
AleDavMart <aleyda.davalos@gmail.com>
2022-05-08 08:44:12 -0700

    Created db model, connection, and DB

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');//will deal with MongoDB
 
 const app = express();
-// creating a router
-const bookRouter = express.Router();
+const db = mongoose.connect('mongodb://localhost/bookAPI');//created a db connection 
+const bookRouter = express.Router(); //Created a router
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel'); // Creating a book model - mongo uses to drive the db
 
 
 bookRouter.route('/books')
-  .get( (req,res)=>{ //like a regular get route
-    const response = { hello: 'This is my API'};
+  .get((req, res) => { //like a regular get route
+    const response = { hello: 'This is my API' };
 
     res.json(response);
   });
 app.use('/api', bookRouter);
-  
+
 app.get('/', (req, res) => {
   res.send('Welcome to my API');
 });
-
 
 
 app.listen(port, () => {
