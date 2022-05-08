a5056c873e382c6748fd886c31ed979acd19fa4a
AleDavMart <aleyda.davalos@gmail.com>
2022-05-08 08:07:59 -0700

    Adding changes

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 
 const app = express();
-
+// creating a router
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
+
+bookRouter.route('/books')
+  .get( (req,res)=>{ //like a regular get route
+    const response = { hello: 'This is my API'};
+
+    res.json(response);
+  });
+app.use('/api', bookRouter);
+  
 app.get('/', (req, res) => {
   res.send('Welcome to my API');
 });
+
+
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
