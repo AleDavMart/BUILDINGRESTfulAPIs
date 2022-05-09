9f0a567c42775c00b9216b33cace04a6eb2bdadb
AleDavMart <aleyda.davalos@gmail.com>
2022-05-08 19:55:28 -0700

    Included body parser in dependencies

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');//will deal with MongoDB
-
+const bodyParser = require ('body-parser');
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');//created a db connection 
 const bookRouter = express.Router(); //Created a router
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel'); // Creating a book model - mongo uses to drive the db
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); //pull JSON from POST body and format in req.body
+
 
 bookRouter.route('/books')
+  .post((req,res)=>{
+    const book = new Book(req.body); //Whatever gets posted to this route will create a new book in DB
+  })
   .get((req, res) => { //query to the mongodb and action to be taken
     const query = {};// creating an empty object for query
-    if(req.query.genre){
+    if (req.query.genre) {
       query.genre = req.query.genre; //filtering queries based on generes, so weird queries will be ignored
     }
     Book.find(query, (err, books) => {
-      if(err) {
+      if (err) {
         return res.send(err);
       }
       return res.json(books);
@@ -25,9 +31,9 @@ app.use('/api', bookRouter);
 
 bookRouter.route('/books/:bookId') //filtering a single book by ID
   .get((req, res) => { //query to the mongodb and action to be taken
-    
+
     Book.findById(req.params.bookId, (err, book) => {
-      if(err) {
+      if (err) {
         return res.send(err);
       }
       return res.json(book);
